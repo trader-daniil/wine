3684d8c7b584178479974080bf19e354b3df5984
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-02-14 10:24:57 +0300

    corrected functions

--- approaching_template.py
+++ approaching_template.py
@@ -1,37 +1,43 @@
-import datetime
+from datetime import datetime
 from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import dateutil
 
-PATH_FILE_WITH_VINES = 'vines_data/wine3.xlsx'
-
-
-def get_company_age(current_date):
-    foundation_year = datetime.datetime(
-        year=1920,
-        month=1,
-        day=1,
-    )
-    delta_years = current_date - foundation_year.date()
-    company_age = round(delta_years.days / 365.25)
-    if str(company_age)[-1] in ('2', '3', '4'):
-        return f'{company_age} года'
-    elif str(company_age)[-1] == '1':
-        return f'{company_age} год'
-    return f'{company_age} лет'
+WINES_FILEPATH = 'vines_data/wine3.xlsx'
 
 
-def parse_vines_from_excel(file):
-    excel_data_df = pandas.read_excel(file)
-    excel_data_df.fillna('', inplace=True)
-    vines = excel_data_df.to_dict(orient='records')
-    parsed_vines = defaultdict(list)
-    for vine in vines:
-        category = vine['Категория']
-        parsed_vines[category].append(vine)
-    return parsed_vines
+YEARS = {
+    '1': 'год',
+    '2': 'года',
+    '3': 'года',
+    '4': 'года',
+    '5': 'лет',
+    '6': 'лет',
+    '7': 'лет',
+    '8': 'лет',
+    '9': 'лет',
+    '0': 'лет',
+}
+
+def get_company_age(founded_at):
+    current_year = datetime.now().year
+    company_age = current_year - founded_at
+    last_number_of_age = str(company_age)[-1]
+    return f'{company_age} {YEARS[last_number_of_age]}'
+
+
+def parse_vines_from_excel(filepath):
+    excel_wines = pandas.read_excel(filepath)
+    excel_wines.fillna('', inplace=True)
+    wines = excel_wines.to_dict(orient='records')
+    grouped_wines = defaultdict(list)
+    for wine in wines:
+        category = wine['Категория']
+        grouped_wines[category].append(wine)
+    return grouped_wines
 
 
 def main():
@@ -39,10 +45,12 @@ def main():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml']),
     )
-    current_date = datetime.date.today()
     template = env.get_template('template.html')
-    company_age = get_company_age(current_date=current_date)
-    vines_categories = parse_vines_from_excel(file=PATH_FILE_WITH_VINES)
+    wines_company_founded_at = 1920
+    company_age = get_company_age(
+        founded_at=wines_company_founded_at
+    )
+    vines_categories = parse_vines_from_excel(filepath=WINES_FILEPATH)
     rendered_page = template.render(
         age=company_age,
         categories=vines_categories,
